0d3134d192711bb1d2c6d0630fd8aaa7b5266db0
Luis Gabriel <janco7249@gmail.com>
2021-10-22 01:07:12 -0400

    style: constants

---
+++ src/styles/constant.js
@@ -0,0 +1,36 @@
+// breakpoints
+export const breakpoints = {
+  mobile: '480px',
+  tablet: '768px',
+  laptop: '1020px',
+  desktop: '1200px'
+}
+// fonts
+
+// colors
+export const colors = {
+  background: '#1f2028',
+  card: '#2e3039',
+  colorWhite: '#eee',
+  colorBlue: '#61dafb',
+  colorBlack: '#2e3039'
+}
+// Skeleton
+export const colorSkeleton = [
+  { num: 0, color: '#168eea' },
+  { num: 1, color: '#629aa9' },
+  { num: 2, color: '#553a99' },
+  { num: 3, color: '#fd9f3e' },
+  { num: 4, color: '#ee4f4f' },
+  { num: 5, color: '#ced7df' },
+  { num: 6, color: '#5ecc62' },
+  { num: 7, color: '#e04646' },
+  { num: 8, color: '#ffcc2f' },
+  { num: 9, color: '#168eea' },
+  { num: 10, color: '#629aa9' },
+  { num: 11, color: '#553a99' },
+  { num: 12, color: '#fd9f3e' },
+  { num: 13, color: '#ee4f4f' },
+  { num: 14, color: '#ced7df' }
+]
+// size and padding
