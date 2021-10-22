dfd1c241651ac6a4b3cadd9a5cea067bf40d25e3
Luis Gabriel <janco7249@gmail.com>
2021-10-22 01:25:50 -0400

    perf: Add new react lazy trends

---
+++ src/components/TrendingSearches/LazyTrending.jsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react'
+import useNearScreen from 'hooks/useNearScreen'
+const TrendingSearches = React.lazy(
+  () => import('./TrendingSearches')
+)
+
+export default function LazyTrending () {
+  const { isNearScreen, fromRef } = useNearScreen({ distance: '0px' })
+  return (
+    <div ref={fromRef}>
+      <Suspense fallback={null}>
+        {isNearScreen ? <TrendingSearches /> : null}
+      </Suspense>
+    </div>
+  )
+}
---
+++ src/components/TrendingSearches/TrendingSearches.jsx
@@ -0,0 +1,12 @@
+// import useNearScreen from 'hooks/useNearScreen'
+import { useEffect, useState } from 'react'
+import { getTrendingTerms } from '../../services/getTrendingTermsService'
+import Category from '../Category/Category'
+// Lo llama LazyTrending dinamicamente
+export default function TrendingSearches () {
+  const [trends, setTrends] = useState([])
+  useEffect(() => {
+    getTrendingTerms().then(setTrends)
+  }, [])
+  return <Category name='Trends' options={trends} />
+}
