2e8709646ea2151125e35386779297c52fa888ec
Luis Gabriel <janco7249@gmail.com>
2021-10-23 00:11:45 -0400

    refactor: changes placeholder & add masonry

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -1,21 +1,37 @@
 import { GifItemSkeleton } from 'components/Gif/GifItem'
 import { ThemeProvider } from 'styled-components'
-import { colorSkeleton } from 'styles/constant'
-import GifContainer from 'components/ListOfGifs/GifContainer'
+// import { colorSkeleton } from 'styles/constant'
+// import GifContainer from 'components/ListOfGifs/GifContainer'
+// import useGlobalGifs from 'hooks/useGlobalGifs'
+import Masonry from 'react-masonry-css'
+import useGifs from 'hooks/useGifs'
+
 function Skeleton () {
+  const { gifs } = useGifs()
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  }
   return (
-    <GifContainer>
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className='my-masonry-grid'
+      columnClassName='my-masonry-grid_column'
+    >
       {
-        colorSkeleton.map(theme => (
-          <ThemeProvider key={theme.num} theme={theme}>
-            <GifItemSkeleton key={theme.num}>
+        gifs.map(theme => (
+          <ThemeProvider key={theme.id} theme={theme}>
+            <GifItemSkeleton key={theme.id}>
               <div className='title' />
               <div className='content' />
             </GifItemSkeleton>
           </ThemeProvider>
         ))
       }
-    </GifContainer>
+    </Masonry>
   )
 }
 
