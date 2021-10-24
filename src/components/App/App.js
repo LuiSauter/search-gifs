c912f9997988ab98a94ae0d4d5a39004db9f9b2e
Luis Gabriel <janco7249@gmail.com>
2021-10-24 15:53:13 -0400

    refactor: add react lazy in home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,13 @@ import { GifsContextProvider } from 'context/GifsContext'
 import StaticContext from 'context/StaticContext'
 
 import Detail from 'pages/Detail/Detail'
-import Home from '../../pages/Home/Home'
 import SearchResults from '../../pages/SearchResults/SearchResults'
 import Navigator from '../Navigator/Navigator'
 import AppContainer from './AppContainer'
+import React, { Suspense } from 'react'
 
 function App () {
+  const homePage = React.lazy(() => import('../../pages/Home/Home'))
   return (
     <StaticContext.Provider value={{
       name: 'midudev',
@@ -16,21 +17,23 @@ function App () {
     }}
     >
       <AppContainer>
-        <Navigator />
-        <GifsContextProvider>
-          <Route
-            component={Home}
-            path='/'
-          />
-          <Route
-            component={SearchResults}
-            path='/search/:keyword'
-          />
-          <Route
-            component={Detail}
-            path='/gif/:id'
-          />
-        </GifsContextProvider>
+        <Suspense fallback={null}>
+          <Navigator />
+          <GifsContextProvider>
+            <Route
+              component={homePage}
+              path='/'
+            />
+            <Route
+              component={SearchResults}
+              path='/search/:keyword'
+            />
+            <Route
+              component={Detail}
+              path='/gif/:id'
+            />
+          </GifsContextProvider>
+        </Suspense>
       </AppContainer>
     </StaticContext.Provider>
   )
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders without crashing', async () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const title = await screen.findByText(/last search/i)
+  expect(title).toBeInTheDocument()
 })
