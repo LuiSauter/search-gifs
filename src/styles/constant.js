86b038b537fef5c6fd043727c7225232e1eecd53
Luis Gabriel <janco7249@gmail.com>
2021-10-25 00:28:33 -0400

    style: update constants colors

--- src/styles/constant.js
+++ src/styles/constant.js
@@ -31,6 +31,16 @@ export const colorSkeleton = [
   { num: 11, color: '#553a99', size: '100px' },
   { num: 12, color: '#fd9f3e', size: '190px' },
   { num: 13, color: '#ee4f4f', size: '90px' },
-  { num: 14, color: '#ced7df', size: '200px' }
+  { num: 14, color: '#ced7df', size: '200px' },
+  { num: 15, color: '#004d73', size: '80px' },
+  { num: 16, color: '#832561', size: '60px' },
+  { num: 17, color: '#553a99', size: '80px' },
+  { num: 18, color: '#bfca02', size: '110px' },
+  { num: 19, color: '#d0d2d3', size: '150px' },
+  { num: 20, color: '#2d364c', size: '200px' },
+  { num: 21, color: '#464646', size: '160px' },
+  { num: 22, color: '#eb2226', size: '240px' },
+  { num: 23, color: '#fd9f3e', size: '110px' },
+  { num: 24, color: '#00d1b2', size: '320px' }
 ]
 // size and padding
