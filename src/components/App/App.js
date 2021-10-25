a34464fa6b0a5e37e7a685e03ba3e87af756699b
Luis Gabriel <janco7249@gmail.com>
2021-10-25 13:11:23 -0400

    add error 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -32,6 +32,10 @@ function App () {
               component={Detail}
               path='/gif/:id'
             />
+            <Route
+              component={() => <h1>404 Error :( </h1>}
+              path='/404'
+            />
           </GifsContextProvider>
         </Suspense>
       </AppContainer>
