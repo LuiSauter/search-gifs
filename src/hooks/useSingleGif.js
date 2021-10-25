da53648955a0e69716ae7a405aa4fd6a5455072d
Luis Gabriel <janco7249@gmail.com>
2021-10-25 13:12:56 -0400

    add useSingleGif.js

---
+++ src/hooks/useSingleGif.js
@@ -0,0 +1,31 @@
+import { getSingleGif } from '../services/getSingleGif'
+import { useEffect, useState } from 'react'
+import useGifs from './useGifs'
+
+// import useGlobalGifs from 'hooks/useGlobalGifs'
+
+export default function useSingleGif ({ id }) {
+  const { gifs } = useGifs()
+  const gifFromCache = gifs.find(singleGif => singleGif.id === id)
+  const [gif, setGifs] = useState(gifFromCache)
+  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (!gif) {
+      setIsLoading(true)
+      getSingleGif({ id })
+        .then(gif => {
+          setGifs(gif)
+          setIsLoading(false)
+          setIsError(false)
+        })
+        .catch(() => {
+          setIsLoading(false)
+          setIsError(true)
+        })
+    }
+  }, [gif, id])
+
+  return { gif, isLoading, isError }
+}
