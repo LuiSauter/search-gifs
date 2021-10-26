af54d154a6ad3e057fdedca6c6a65b66be730745
Luis Gabriel <janco7249@gmail.com>
2021-10-26 01:45:31 -0400

    refactor: add constants and change props

--- src/services/getTrendingTermsService.js
+++ src/services/getTrendingTermsService.js
@@ -3,7 +3,26 @@ import config from 'config'
 const { API_KEY, API_URL } = config
 const fromApiResponseToGifs = apiResponse => {
   const { data = [] } = apiResponse
-  return data
+  // const trends = { ...data }
+
+  const name = data.map(trend => {
+    return { trend: trend }
+  })
+
+  const colores = { colorsNum: data.length, colors: [] }
+  const generateColor = () => {
+    return '#' + Math.random().toString(16).substr(-6)
+  }
+  for (let i = 0; i < colores.colorsNum; i += 1) {
+    colores.colors.push({ hexCode: generateColor(), id: i })
+  }
+
+  const themeColors = colores.colors.map(colors => {
+    name[colors.id].color = colors.hexCode
+    const { color, trend } = name[colors.id]
+    return { color, trend }
+  })
+  return themeColors
 }
 
 export const getTrendingTerms = async () => {
