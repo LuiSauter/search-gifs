0202d4ec183924ec408953b005b68562070e228a
Luis Gabriel <janco7249@gmail.com>
2021-10-26 22:49:11 -0400

    refactor: changes props returned

--- src/services/getSingleGif.js
+++ src/services/getSingleGif.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 import config from 'config'
-// import { colorSkeleton } from 'styles/constant'
 
 const { API_URL, API_KEY } = config
 
 const fromApiResponseToGif = apiResponse => {
   const { data } = apiResponse
   const theme = { ...data }
+  const gif = { ...data }
   theme.theme = { num: 0, color: '#2d364c', size: '200px' }
   const { id, title, images, type } = theme
   const { url, height } = images.original
   const heightGif = `${height}px`
-  return { id, title, url, heightGif, type, theme }
+  return { id, title, url, heightGif, type, theme, gif }
 }
 
 export const getSingleGif = async ({ id }) => {
