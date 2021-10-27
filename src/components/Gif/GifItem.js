48c9d2dcf29430fdab3f7a68f6d363fda3bd14ff
Luis Gabriel <janco7249@gmail.com>
2021-10-26 22:31:26 -0400

    style: changes props theme and height

--- src/components/Gif/GifItem.js
+++ src/components/Gif/GifItem.js
@@ -4,43 +4,41 @@ import { breakpoints } from 'styles/constant'
 export const GifItem = styled.div`
   display: flex;
   width: 100%;
-  /* height: ${props => props.theme.heightGif}; */
   max-height: 400px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
   background: ${props => props.theme.theme.color};
-  border: 2px solid ${props => props.theme.theme.color};
   transition: transform .2s ease-in-out;
   overflow: hidden;
   position: relative;
   a {
+    height: ${props => props.theme.heightGif};
     margin: auto;
     width: 100%;
-    text-align: center;
+    text-align: left;
   }
-
   &:hover {
+    border:1px solid ${props => props.theme.theme.color};
     transform: scale(1.01);
     h4 {
       display: block;
-      background: linear-gradient(180deg,rgb(0 0 0) 0%,rgb(0 0 0 / 0%) 90%);
+      background: ${props => props.theme.theme.color}80;
+      filter: contrast(2);
     }
   }
-
   h4 {
     display: none;
     position: absolute;
     top: 0;
-    font-size: .7rem;
+    font-size: 1rem;
     font-weight: 500;
-    padding: .7rem .5rem;
+    padding: .4rem .5rem;
     width: 100%;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
   }
-
   img {
     height: ${props => props.theme.heightGif};
     /* min-height: 350px; */
@@ -51,8 +49,6 @@ export const GifItem = styled.div`
     border-bottom-right-radius: 10px;
     cursor: pointer;
   }
-
-
   @media screen and (max-width: ${breakpoints.mobile}) {
     width: 100%;
   }
