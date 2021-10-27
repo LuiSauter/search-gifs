b80d81b9a5f660cd88e0e982e24386c372d5efec
Luis Gabriel <janco7249@gmail.com>
2021-10-26 22:33:56 -0400

    style: change margin

--- src/components/Navigator/Nav.js
+++ src/components/Navigator/Nav.js
@@ -2,12 +2,10 @@ import styled from 'styled-components'
 
 const Nav = styled.nav`
   text-align: center;
-  /* padding: 0.5em; */
-  margin: 1.7rem 0;
   img {
     cursor: pointer;
     width: 100px;
+    margin: 1.7rem 0;
   }
-
 `
 export default Nav
