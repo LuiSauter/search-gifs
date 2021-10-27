62717d6d64ca6f474f49b3c9d364fcf23cc01cd3
Luis Gabriel <janco7249@gmail.com>
2021-10-26 22:40:51 -0400

    style: changes width skeleton

--- src/components/Skeleton/SkeletonGifs.js
+++ src/components/Skeleton/SkeletonGifs.js
@@ -2,27 +2,24 @@ import styled from 'styled-components'
 import { breakpoints } from 'styles/constant'
 
 export const SkeletonGif = styled.div`
-  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
   background: ${(props) => props.theme.color};
-
+  margin: auto;
   .title {
     height: 18px;
-    width: 95%;
+    width: 100%;
     border-radius: 10px;
     margin-bottom: 1.3rem;
   }
-
   .content {
-    width: 95%;
+    width: 100%;
     height: ${(props) => props.theme.size};
     border-radius: 10px;
   }
-
   @media screen and (max-width: ${breakpoints.mobile}) {
     width: 100%;
   }
