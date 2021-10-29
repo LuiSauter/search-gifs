16d23f080289c050d24738ce963b0af880989265
Luis Gabriel <janco7249@gmail.com>
2021-10-28 23:29:23 -0400

    feat: add new route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
+import React, { Suspense } from 'react'
 import { Route, Switch } from 'wouter'
 import { GifsContextProvider } from 'context/GifsContext'
+
+import { UserContextProvider } from 'context/UserContext'
 
 import Detail from 'pages/Detail/Detail'
 import SearchResults from '../../pages/SearchResults/SearchResults'
 import Navigator from '../Navigator/Navigator'
 import AppContainer from './AppContainer'
-import React, { Suspense } from 'react'
 import ErrorPage from '../../pages/ErrorPage/index'
 import HeaderLg from 'components/Navigator/Header'
-import Login from '../../pages/Login/Login'
-import { UserContextProvider } from 'context/UserContext'
+import LoginPage from 'pages/LoginPage/LoginPage'
 
 function App () {
   const homePage = React.lazy(() => import('../../pages/Home/Home'))
@@ -25,7 +26,7 @@ function App () {
               <Route component={homePage} path='/' />
               <Route component={SearchResults} path='/search/:keyword/:rating?' />
               <Route component={Detail} path='/gif/:id' />
-              <Route component={Login} path='/login' />
+              <Route component={LoginPage} path='/login' />
               <Route component={ErrorPage} path='/:rest*' />
             </Switch>
           </GifsContextProvider>
