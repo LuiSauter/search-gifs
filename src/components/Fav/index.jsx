04a701e64d904550e609ae94cfea25dddbe50b3a
Luis Gabriel <janco7249@gmail.com>
2021-10-28 23:29:54 -0400

    rename file

--- src/components/Fav/index.jsx
+++ src/components/Fav/index.jsx
@@ -2,23 +2,24 @@ import useUser from 'hooks/useUser'
 import { useLocation } from 'wouter'
 
 function Fav ({ id }) {
-  const { isLogged, addFav, favs } = useUser({ id })
+  const { isLogged, favs, addFav } = useUser()
   const [, navigate] = useLocation()
-
-  const isFaved = favs.some(favId => favId === id)
+  const isFaved = favs.find(favId => favId === id)
 
   const handleClick = () => {
     if (!isLogged) return navigate('/login')
     addFav({ id })
-    console.log(id)
   }
 
-  const [label, emoji] = isFaved
+  const [label, emoji] = typeof isFaved !== 'undefined'
     ? ['Remove Gif from favorites', 'X']
     : ['Add gif to favorites', 'fav']
+
   return (
     <button onClick={handleClick}>
-      <span role='img' aria-label={label}>{emoji}</span>
+      <span role='img' aria-label={label}>
+        {emoji}
+      </span>
     </button>
   )
 }
