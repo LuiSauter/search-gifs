bb8cfeb6a6073e05c817b85349da12a7f6748499
Luis Gabriel <janco7249@gmail.com>
2021-10-29 23:43:41 -0400

    style: change padding masonry

--- src/components/ListOfGifs/GifContainer.js
+++ src/components/ListOfGifs/GifContainer.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
 const GifContainer = styled.div`
-  display: grid;
+  /* display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-template-rows: masonry;
   grid-auto-flow: row dense;
   align-items: center;
   justify-content: center;
   gap: 20px;
-  margin: 1.7rem auto;
+  margin: 1.7rem auto; */
 `
 
 export default GifContainer
