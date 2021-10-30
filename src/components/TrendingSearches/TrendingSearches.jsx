8373568c0f15684cf3da64b64ed62f01371a4c23
Luis Gabriel <janco7249@gmail.com>
2021-10-29 23:45:38 -0400

    fix: update component

--- src/components/TrendingSearches/TrendingSearches.jsx
+++ src/components/TrendingSearches/TrendingSearches.jsx
@@ -5,7 +5,6 @@ import Category from '../Category/Category'
 // Lo llama LazyTrending dinamicamente
 export default function TrendingSearches () {
   const [trends, setTrends] = useState([])
-  // const [loading, setLoading] = useState(false)
   useEffect(() => {
     getTrendingTerms()
       .then(setTrends)
