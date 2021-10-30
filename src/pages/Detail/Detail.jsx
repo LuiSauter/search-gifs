5d8ec256b0aed4f9f5244a3f2237c14e93bcac2a
Luis Gabriel <janco7249@gmail.com>
2021-10-29 23:40:11 -0400

    style: update ui detail page

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -3,6 +3,7 @@ import useSingleGif from 'hooks/useSingleGif'
 import { Redirect } from 'wouter'
 import { Helmet } from 'react-helmet'
 import { ThemeProvider } from 'styled-components'
+import Fav from 'components/Fav'
 
 function Detail ({ params }) {
   const { gif, isLoading, isError } = useSingleGif({ id: params.id })
@@ -29,11 +30,13 @@ function Detail ({ params }) {
               <img
                 loading='lazy'
                 src={gif.gif.images.downsized_medium.url}
-                // width={gif.gif.images.downsized_medium.width}
                 alt={title}
               />
             </div>
-            <a href={gif.url} target='_blank' rel='noreferrer'>See more</a>
+            <div className='detail-container__btn'>
+              <Fav id={gif.id} />
+              <a href={gif.url} target='_blank' rel='noreferrer'>link <i className='fas fa-angle-double-right' /></a>
+            </div>
           </div>
         </DetailItem>
       </ThemeProvider>
--- src/pages/Detail/DetailItem.js
+++ src/pages/Detail/DetailItem.js
@@ -24,18 +24,48 @@ const DetailItem = styled.div`
     h4 {
       margin: auto;
     }
-    a {
-      text-align: center;
-      font-size: 1rem;
-      border: 1px solid ${colors.colorBlue};
-      color: ${colors.colorBlue};
-      border-radius: 7px;
-      margin-top: 1rem;
-      padding: .5rem .5rem;
-      transition: background .3s ease-in-out;
-      &:hover {
-        color: ${colors.background};
-        background: ${colors.colorBlue};
+
+    &__btn {
+      margin: 1rem auto;
+      display: flex;
+      flex-direction: row;
+      align-items:center;
+      justify-content: center;
+      a {
+        text-align: center;
+        /* border: 1px solid ${colors.colorBlue}; */
+        color: ${colors.colorWhite};
+        border-radius: 7px;
+        transition: background .3s ease-in-out;
+        font-size: 1.8rem;
+        padding: .2rem .5rem;
+        margin-left: 1rem;
+        &:hover {
+          color: ${colors.background};
+          background: ${colors.colorBlue};
+        }
+      }
+      button {
+        border: none;
+        background-color: transparent;
+        i {
+          font-size: 1.8rem;
+          color: ${colors.colorWhite};
+          transition: transform .5s;
+          &:hover {
+            color: #FF5151;
+            /* font-size: 1.6rem; */
+            transform: scale(1.09);
+          }
+        }
+        .heart {
+          color: #FF5151;
+          transition: filter .5s ;
+          &:hover {
+            color: #FF5151;
+            filter: drop-shadow(0px 0px 4px #ee505066);
+          }
+        }
       }
     }
   }
