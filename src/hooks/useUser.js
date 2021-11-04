e427cecf00cbf98cd4112ff799e0a3914a263752
Luis Gabriel <janco7249@gmail.com>
2021-11-03 21:50:50 -0400

    perf: changes request singup and signin

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,55 +2,74 @@ import { useCallback, useContext, useState } from 'react'
 import { loginService } from 'services/loginService'
 import Context from 'context/UserContext'
 import { addFavService } from 'services/addFavService'
-import { deleteFav } from 'services/deleteFav'
+import { registerService } from 'services/Register'
 
 export default function useUser () {
-  const { jwt, setJWT, favs, setFavs } = useContext(Context)
+  const { email, setEmail, favs, setFavs, msg204, setMsg204, setJWT, JWT } = useContext(Context)
   const [state, setState] = useState({ loading: false, error: false })
 
-  const login = useCallback(({ username, password }) => {
+  const signupForm = useCallback((data) => {
+    registerService(data).then(res => {
+      if (res.message === 'The email is already in use.') {
+        window.sessionStorage.setItem('signup204', res.message)
+        return setMsg204(res.message)
+      }
+      window.sessionStorage.setItem('signup', res.email)
+      window.sessionStorage.setItem('signup204', res.message)
+      window.sessionStorage.setItem('signup204', res.message)
+      setMsg204(res.message)
+      return setEmail(res.email)
+    })
+  }, [setEmail, setMsg204])
+
+  const login = useCallback((data) => {
     setState({ loading: true, error: false })
-    loginService({ username, password })
-      .then(jwt => {
-        window.sessionStorage.setItem('jwt', jwt)
+    loginService(data)
+      .then(res => {
+        if (res.message === 'Successfuly Authenticated') {
+          window.localStorage.setItem('jwt', res.token)
+          return setJWT(res.token)
+        }
+        setJWT(null)
+        window.localStorage.removeItem('jwt')
         setState({ loading: false, error: false })
-        setJWT(jwt)
       }).catch(err => {
-        window.sessionStorage.removeItem('jwt')
+        window.localStorage.removeItem('jwt')
         setState({ loading: false, error: true })
         console.log(err)
       })
   }, [setJWT])
 
-  const addFav = useCallback(({ id }) => {
-    const favExist = favs.some(favos => favos.fav === id)
-    const favorito = favs.find(fa => fa.fav === id)
-    if (favExist) {
-      const newFavs = favs.filter(fav => fav.fav !== id)
-      setFavs(newFavs)
-      deleteFav({ id: favorito.id, jwt })
-    } else {
-      addFavService({ id, jwt })
-        .then(favorite => {
-          const newFav = {
-            fav: favorite.fav,
-            id: favorite.id
-          }
-          setFavs(prevFav => [...prevFav, newFav])
-        })
-        .catch(err => console.error(err))
-    }
-  }, [favs, jwt, setFavs])
+  const addFav = useCallback(({ id, title }) => {
+    addFavService({ id, JWT, title })
+      .then(favorite => {
+        if (favorite.message === true) {
+          const newFavs = favs.filter(fav => fav.fav !== id)
+          return setFavs(newFavs)
+        }
+        const newFav = {
+          fav: favorite.fav,
+          title: favorite.title,
+          id: favorite.id
+        }
+        setFavs(prevFav => [newFav, ...prevFav])
+      })
+      .catch(err => console.error(err))
+  }, [JWT, favs, setFavs])
 
   const logout = useCallback(() => {
-    window.sessionStorage.removeItem('jwt')
     setJWT(null)
+    window.localStorage.removeItem('jwt')
   }, [setJWT])
 
   return {
-    addFav,
+    email,
+    msg204,
+    setMsg204,
+    isLogged: Boolean(JWT),
     favs,
-    isLogged: Boolean(jwt),
+    addFav,
+    signupForm,
     login,
     logout,
     isLoginLoading: state.loading,
